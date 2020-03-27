41959378dfca0acf0dbb8209ab39b62df08e8e81
hamza bellamine <hamza.bellamine@gmail.com>
2020-03-27 23:58:15 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Prescription.destroy_all
 Ordonnance.destroy_all
 Consultation.destroy_all
 Patient.destroy_all
