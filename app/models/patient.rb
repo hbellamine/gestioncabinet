8f6f2b456a981065002b1d696c32323c7968d8da
hamza bellamine <hamza.bellamine@gmail.com>
2020-04-06 17:44:50 +0100

    searchbyfirstname

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -6,7 +6,7 @@ class Patient < ApplicationRecord
 
     include PgSearch::Model
   pg_search_scope :search_by_nom_and_cin,
-    against: [ :nom, :cin ],
+    against: [ :nom, :prenom, :cin ],
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
     }
